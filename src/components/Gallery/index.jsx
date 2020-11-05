117530cf7e9e982366e065a085cd0611f6ee9a1c
katieatgeorgian <47111708+katieatgeorgian@users.noreply.github.com>
2020-11-04 19:55:51 -0500

    add modal

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,41 +1,28 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { Card } from 'react-bootstrap';
 
-// import GalleryCard from '../shared/Card';
+
 import Footer from '../shared/Footer';
 import Styles from './styles';
 import Button from '../shared/Button';
+import ModalGallery from '../shared/Modal';
 
 let data = require("../../dummyData.json");
 console.log(data);
 
 const Gallery = () => {
 
-  useEffect(() => {
-    /* gallery images */
-    var flipCard = document.querySelectorAll(".flip-card");
-    var flipInner = [...document.querySelectorAll(".flip-card-inner")];
+  const [open, setOpen] = useState(false);
 
-    for (var i = 0; i < flipCard.length; i++) {
-      flipCard[i].addEventListener("mouseover", flip(i));
-    }
-
-    function flip(i) {
-      return function() {
-        flipInner[i].style.transform = "rotateY(180deg)";
-      };
-    }
-
-    for (var i = 0; i < flipCard.length; i++) {
-      flipCard[i].addEventListener("mouseout", flipBack(i));
-    }
-
-    function flipBack(i) {
-      return function() {
-        flipInner[i].style.transform = "rotateY(0deg)";
-      };
-    }
-  });
+  const handleClose = (index) => {
+    console.log('closing');
+    setOpen(false);
+    console.log(open);
+  }
+  const handleShow = (index) => {
+      setOpen(true);
+      console.log('open says me');
+  }
 
 
   return (
@@ -45,36 +32,22 @@ const Gallery = () => {
         {Object.values(data).map((art, i) => {
         console.log(art);
           return (
-              <div key={i} className="flip-card">
-                <div className="flip-card-inner">
-                  <div className="flip-card-front">
+            <>
+              {open && 
+                <ModalGallery show={open} artist={art.artist} title={art.title} category={art.category} image={art.img} close={handleClose}></ModalGallery>
+                }
+              <div data-key={i} key={i} className="galleryCard" onClick={handleShow}>
+ 
                     <img
                       alt={art.title}
                       src={art.img}
+                      style={{width: "75%"}}
                     />
                     <h4>{art.title}</h4>
-                  </div>
-                  <div className="flip-card-back">
-                    <p>Artist: {art.artist}</p>
-                    <p>Email: {art.email}</p>
-                    <Button className="flip-button" href='mailto:${art.email}' label="Contact Artist"></Button>
-                  </div>
+               
                 </div>
-              </div>
-             /* <>
-             <div key={i} className="box">
-              <img
-                alt={art.title}
-                src={art.img}
-              />
-              <h4>{art.title}</h4>
-           
-              <div className="hid-box">
-                <p>`Artist: {art.artist}`</p>
-                <a className="flip-button" href='mailto:${art.email}'>Contact Artist</a>
-              </div>
-              </div>
-              </> */
+                
+                </>
           );
             
         })}
--- src/components/Gallery/styles.jsx
+++ src/components/Gallery/styles.jsx
@@ -9,63 +9,40 @@ export default {
       display:flex;
       flex-wrap: wrap;
     }
-    div.card {
-      width: 40% !important;
+    /* div.card {
+      width: 39% !important;
       margin: 1rem;
-    }
-    div.card-body {
-      text-align: center;
-    }
+    } */
 
-    /* Flip Cards */
-.flip-card {
+
+    /* Cards */
+.galleryCard {
 
   height: 35vh;
   width: 40% !important;
       margin: 1rem;
   text-align: center;
   background-color: transparent;
-  perspective: 1000px;
-}
-
-.flip-card-inner {
-  position: relative;
-  width: 100%;
-  height: 100%;
   text-align: center;
-  transition: transform 0.6s;
-  transform-style: preserve-3d;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+
 }
 
-.flip-card img {
+
+.galleryCard img {
   margin-top: 1rem;
-  width: 75%;
+  /* width: 75%; */
   height: auto;
 }
 
-.flip-card-front,
+/* .flip-card-front,
 .flip-card-back {
   position: absolute;
   width: 100%;
   height: 100%;
   -webkit-backface-visibility: hidden;
   backface-visibility: hidden;
-}
+} */
 
-.flip-card-front {
-  background-color: transparent;
-  color: #535353;
-  line-height: 136px;
-  padding: 10px;
-  vertical-align: middle;
-}
-
-.flip-card-back {
-  background-color: transparent;
-  color: #560829;
-  transform: rotateY(180deg);
-  padding-top: 10%;
-    }
   `,
 };
